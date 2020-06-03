5cbbd560e1a4dd4067c4cfa3c9f7cd8eee2db497
Stephen Hulme <sh@saao.ac.za>
2020-06-03 16:56:01 +0200

    Add basic speed gauge using Py-RailDriver

--- application.py
+++ application.py
@@ -17,18 +17,19 @@ Updated 13th April 2018
 
 """
 
-# Start with a basic flask app webpage.
+import raildriver
+
 from flask_socketio import SocketIO
 from flask import Flask, render_template
 
 from random import random
 from threading import Event, Thread
 
-__author__ = 'slynn'
+__author__ = "rollcloud"
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
-app.config['DEBUG'] = True
+app.config["SECRET_KEY"] = "An actually secret key! Nah, who am I kidding :P"
+app.config["DEBUG"] = True
 
 # turn the flask app into a socketio app
 socketio = SocketIO(app, async_mode=None, logger=True, engineio_logger=True)
@@ -37,32 +38,43 @@ socketio = SocketIO(app, async_mode=None, logger=True, engineio_logger=True)
 thread = Thread()
 thread_stop_event = Event()
 
+# setup RailDriver
+rd = raildriver.RailDriver()
+rd.set_rail_driver_connected(True)  # start data exchange
+
+print(dict(rd.get_controller_list()).values())
+
+number = 42
 
 def generate_numbers():
     """
     Generate a random number every 1 second and emit to a socketio instance (broadcast)
     Ideally to be run in a separate thread?
     """
+    global number
+
     # infinite loop of magical random numbers
     print("Making random numbers")
     while not thread_stop_event.isSet():
-        number = round(random() * 10, 3)
-        print(number)
-        socketio.emit('newnumber', {'number': number}, namespace='/test')
-        socketio.sleep(1)
+        try:
+            number = rd.get_current_controller_value("SpeedometerMPH")
+        except ValueError as err:
+            number += round(random() * 5-2.5, 0)
+        socketio.emit("newnumber", {"number": number}, namespace="/test")
+        socketio.sleep(0.1)
 
 
-@app.route('/')
+@app.route("/")
 def index():
     # only by sending this page first will the client be connected to the socketio instance
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-@socketio.on('connect', namespace='/test')
+@socketio.on("connect", namespace="/test")
 def test_connect():
     # need visibility of the global thread object
     global thread
-    print('Client connected')
+    print("Client connected")
 
     # Start the random number generator thread only if the thread has not been started before.
     if not thread.isAlive():
@@ -70,10 +82,10 @@ def test_connect():
         thread = socketio.start_background_task(generate_numbers)
 
 
-@socketio.on('disconnect', namespace='/test')
+@socketio.on("disconnect", namespace="/test")
 def test_disconnect():
-    print('Client disconnected')
+    print("Client disconnected")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     socketio.run(app)
--- static/js/application.js
+++ static/js/application.js
@@ -1,21 +1,46 @@
-$(document).ready(function(){
+google.charts.load("current", { packages: ["gauge"] });
+google.charts.setOnLoadCallback(main);
+
+var chart, data, options;
+
+function createGauges() {
+    data = google.visualization.arrayToDataTable([
+        ["Label", "Value"],
+        ["Speed", 80],
+        ["Boiler", 55],
+        ["Water", 68],
+    ]);
+
+    options = {
+        width: 400,
+        height: 120,
+        redFrom: 90,
+        redTo: 100,
+        yellowFrom: 75,
+        yellowTo: 90,
+        minorTicks: 5,
+    };
+
+    chart = new google.visualization.Gauge(
+        document.getElementById("chart_div")
+    );
+}
+
+function main() {
+    createGauges();
+
     //connect to the socket server.
-    var socket = io.connect('http://' + document.domain + ':' + location.port + '/test');
-    var numbers_received = [];
+    var socket = io.connect(
+        "http://" + document.domain + ":" + location.port + "/test"
+    );
 
     //receive details from server
-    socket.on('newnumber', function(msg) {
-        console.log("Received number" + msg.number);
-        //maintain a list of ten numbers
-        if (numbers_received.length >= 10){
-            numbers_received.shift()
-        }
-        numbers_received.push(msg.number);
-        numbers_string = '';
-        for (var i = 0; i < numbers_received.length; i++){
-            numbers_string = numbers_string + '<p>' + numbers_received[i].toString() + '</p>';
-        }
-        $('#log').html(numbers_string);
-    });
+    socket.on("newnumber", function (msg) {
+        console.log("Received: " + msg.number);
 
-});
+        // update gauge
+        data.setValue(0, 1, (msg.number + 120) % 120);
+
+        chart.draw(data, options);
+    });
+}
